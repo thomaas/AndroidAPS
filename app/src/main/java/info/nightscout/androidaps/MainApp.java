d4ec8cac6572fadffc1befe1791c90505d2c1065
Tebbe Ubben <tebbe@ubben.eu>
2019-07-11 17:03:43 +0200

    Subscribe on IO thread

--- app/src/main/java/info/nightscout/androidaps/MainApp.java
+++ app/src/main/java/info/nightscout/androidaps/MainApp.java
@@ -90,6 +90,7 @@ import info.nightscout.androidaps.receivers.NSAlarmReceiver;
 import info.nightscout.androidaps.services.Intents;
 import info.nightscout.androidaps.utils.FabricPrivacy;
 import io.fabric.sdk.android.Fabric;
+import io.reactivex.schedulers.Schedulers;
 
 import static info.nightscout.androidaps.plugins.general.versionChecker.VersionCheckerUtilsKt.triggerCheckVersion;
 
@@ -122,7 +123,7 @@ public class MainApp extends Application {
     public void onCreate() {
         super.onCreate();
         AppRepository.INSTANCE.initialize(this);
-        AppRepository.INSTANCE.getProperGlucoseValuesInTimeRange(TimeUnit.DAYS.toMillis(1)).subscribe(values -> {
+        AppRepository.INSTANCE.getProperGlucoseValuesInTimeRange(TimeUnit.DAYS.toMillis(1)).subscribeOn(Schedulers.io()).subscribe(values -> {
             DatabaseHelper.scheduleBgChange();
         });
         log.debug("onCreate");
